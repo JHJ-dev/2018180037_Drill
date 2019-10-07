71010896e13a09ef5911416bd35a67d924c94771
JHJ <wjs3662@naver.com>
2019-10-07 19:13:03 +0900

    Update drill5.py

--- Drill-05/drill5.py
+++ Drill-05/drill5.py
@@ -13,25 +13,26 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            x1, y1 = event.x1, KPU_HEIGHT - 1 - event.y1
+            x1, y1 = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-cursor = load_image('hand_arrow.png')
-
+hand = load_image('hand_arrow.png')
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
-    
+
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    cursor.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.clip_draw(1, 1, 50, 52, x, y)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x1, y1)
     update_canvas()
     frame = (frame + 1) % 8
