5233350cab32602a77b62742257bd7eabac11415
JHJ <wjs3662@naver.com>
2019-10-21 18:51:03 +0900

    Drill08
    2018180037 전혜진

---
+++ Drill-08/pause_plus.py
@@ -0,0 +1,56 @@
+import game_framework
+import main_state
+from pico2d import *
+
+class Pause_Plus:
+    def __init__(self):
+        self.image = load_image('pause.png')
+        self.time = 0
+
+    def update(self):
+        self.time = 1 - self.time
+        delay(0.1)
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300, 500, 500)
+
+
+def enter():
+    global pause
+    pause = Pause_Plus()
+
+
+def exit():
+    global pause
+    del pause
+
+
+def update():
+    global pause
+    pause.update()
+
+
+def draw():
+    global pause
+    clear_canvas()
+    main_state.draw()
+    pause.update()
+    pause.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_q:
+            game_framework.pop_state()
+
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
