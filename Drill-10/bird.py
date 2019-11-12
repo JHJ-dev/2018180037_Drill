e2a59b07c9cad99f4dc7a13596333c2dfb8a9fda
JHJ <wjs3662@naver.com>
2019-11-12 22:08:11 +0900

    Drill10 완성(2018180037 전혜진)
    커밋횟수 : 3회
    https://youtu.be/eAK5ZR77Ssw

--- Drill-10/bird.py
+++ Drill-10/bird.py
@@ -1,8 +1,6 @@
 import game_framework
 from pico2d import *
 
-import game_world
-
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
 RUN_SPEED_KMPH = 30.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
@@ -11,10 +9,9 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+FRAMES_PER_ACTION = 14
 
 class Bird:
-
     def __init__(self):
         self.x, self.y = 1600 // 2, 300
         self.image = load_image('bird_animation.png')
@@ -23,17 +20,17 @@ class Bird:
         self.velocity = RUN_SPEED_PPS
         self.frame_x = 0
         self.frame_y = 0
-        self.frame = 0
+        self.frame_time = 0
 
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
-        self.frame_x = int(self.frame) % 5
-        if int(self.frame) % 14 == 0:
+        self.frame_time = (self.frame_time + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
+        self.frame_x = int(self.frame_time) % 5
+        if int(self.frame_time) % 14 == 0:
             self.frame_y = 2
-        if int(self.frame) % 14 == 5:
+        elif int(self.frame_time) % 14 == 5:
             self.frame_y = 1
-        if int(self.frame) % 14 == 10:
+        elif int(self.frame_time) % 14 == 10:
             self.frame_y = 0
 
         self.x += self.velocity * game_framework.frame_time
